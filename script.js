90c6c65d31697647bb3021620f2408dbcd5f1fbe
RUCHIR04-R <ruchirgawture@gmail.com>
2025-02-24 10:21:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-function insert_Row() {
-    //Write your code here
-  
-  
-}
+document.getElementById("insertBtn").addEventListener("click", function() {
+    
+    var table = document.getElementById("myTable");
+    
+    var newRow = table.insertRow(0);
+    
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    
+    cell1.innerHTML = "New Cell1";
+    cell2.innerHTML = "New Cell2";
+});
